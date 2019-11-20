5e05aa3c4a553be9d6d22dc9cdd053163004bece
Alvaro Ariño Cabau <alvaro.arino.cabau@gmail.com>
2019-11-20 12:29:47 +0100

    Initial Commit

---
+++ game19/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game19
+//
+//  Created by Alvaro Ariño Cabau on 20/11/2019.
+//  Copyright © 2019 Alvaro Ariño Cabau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
