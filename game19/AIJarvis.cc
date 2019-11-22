140ab25b6cc47b0770d87aec2f0e0286e66b1c1c
Alvaro Ariño Cabau <alvaro.arino.cabau@gmail.com>
2019-11-22 23:01:21 +0100

    Gano a la DEMO

--- game19/AIJarvis.cc
+++ game19/AIJarvis.cc
@@ -1,6 +1,5 @@
 #include "Player.hh"
 
-
 /**
  * Write the name of your player and save this file
  * with the same name and .cc extension.
@@ -9,25 +8,118 @@
 
 
 struct PLAYER_NAME : public Player {
-
-  /**
-   * Factory: returns a new instance of this class.
-   * Do not modify this function.
-   */
-  static Player* factory () {
-    return new PLAYER_NAME;
-  }
-
-  /**
-   * Types and attributes for your player can be defined here.
-   */
-
-  /**
-   * Play method, invoked once per each round.
-   */
-  virtual void play () {
-  }
-
+    
+    /**
+     * Factory: returns a new instance of this class.
+     * Do not modify this function.
+     */
+    static Player* factory () {
+        return new PLAYER_NAME;
+    }
+    
+    /**
+     * Types and attributes for your player can be defined here.
+     */
+    
+    void BFS_dwarves(int id) {
+        Pos origen = unit(id).pos;
+        
+        vector< vector<bool> > visited(rows(), vector<bool>(cols(), false));
+        vector<Dir> directions = {Bottom, BR, Right, RT, Top, TL, Left, LB};
+        
+        queue<Pos> q;
+        
+        visited[origen.i][origen.j] = true;
+        q.push(origen);
+        
+        // 'i' will be used to get all adjacent
+        // vertices of a vertex
+        
+        while (not q.empty()) {
+            // Dequeue a vertex from queue and print it
+            Pos p = q.front();
+            q.pop();
+            
+            // Get all adjacent vertices of the dequeued
+            // vertex s. If a adjacent has not been visited,
+            // then mark it visited and enqueue it
+            for (Dir d : directions) {
+                Pos p1 = p + d;
+                if (pos_ok(p1) and !visited[p1.i][p1.j]) {
+                    visited[p1.i][p1.j] = true;
+                    q.push(p1);
+                    if (cell(p1).type == Outside or cell(p1).type == Cave) {
+                        if (cell(p1).id == -1 or
+                            cell(p1).treasure or
+                            (cell(p1).id != -1 and
+                                (unit(cell(p1).id).type == Orc or unit(cell(p1).id).player != me()))) {
+                            command(id, d);
+                            return;
+                        }
+                    } else if (cell(p1).type == Rock) {
+                        command(id, d);
+                    }
+                }
+            }
+        }
+    }
+    
+    void BFS_wizards(int id) {
+        Pos origen = unit(id).pos;
+        
+        vector< vector<bool> > visited(rows(), vector<bool>(cols(), false));
+        vector<Dir> directions = {Bottom, Right, Top, Left};
+        
+        queue<Pos> q;
+        
+        visited[origen.i][origen.j] = true;
+        q.push(origen);
+        
+        // 'i' will be used to get all adjacent
+        // vertices of a vertex
+        
+        while (not q.empty()) {
+            // Dequeue a vertex from queue and print it
+            Pos p = q.front();
+            q.pop();
+            
+            // Get all adjacent vertices of the dequeued
+            // vertex s. If a adjacent has not been visited,
+            // then mark it visited and enqueue it
+            for (Dir d : directions) {
+                Pos p1 = p + d;
+                if (pos_ok(p1) and !visited[p1.i][p1.j]) {
+                    visited[p1.i][p1.j] = true;
+                    q.push(p1);
+                    if (cell(p1).id == -1 or not cell(p1).treasure) {
+                        command(id, d);
+                        return;
+                    } else if (cell(p1).id != -1 and unit(cell(p1).id).player == me()) {
+                        command(id, None);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Play method, invoked once per each round.
+     */
+    virtual void play () {
+        vector<int> my_dwarves = dwarves(me());
+        vector<int> dwarR = random_permutation(my_dwarves.size());
+        for (int d: dwarR) {
+            BFS_dwarves(my_dwarves[d]);
+        }
+        
+        vector<int> my_wizards = wizards(me());
+        vector<int> wizR = random_permutation(my_wizards.size());
+        for (int w: wizR) {
+            BFS_wizards(my_wizards[w]);
+        }
+    }
+    
 };
 
 
